176779ca597dc2489661a0798e99577f3b3e90b4
Полина <Shumilina246@gmail.com>
2021-04-12 17:35:32 +0300

    fix: launch & sound

--- src/sound.py
+++ src/sound.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
+import rospy
 import pyglet
-song = pyglet.media.load('/home/pi/sounds/serdce.mp3')
-song.play()
-pyglet.app.run()
+
+def callback(data):
+    song = pyglet.media.load('/home/pi/sounds/serdce.mp3')
+    song.play()
+    pyglet.app.run()
+
+def listener():
+	rospy.init_node('sound', anonymous=True)
+	rospy.Subscriber("chatter", String, callback)
+
+if __name__ == '__main__':
+    listener()
+
